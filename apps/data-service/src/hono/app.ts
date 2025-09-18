7f8d9d5913a780d5198b2f8193ff149b68430490
lenryk <lenry101@gmail.com>
2025-09-18 23:30:59 +0100

    add geobased logic

--- apps/data-service/src/hono/app.ts
+++ apps/data-service/src/hono/app.ts
@@ -1,16 +1,25 @@
+import { getDestinationForCountry } from '@/helpers/route-ops';
+import { getLink } from '@repo/data-ops/queries/links';
+import { cloudflareInfoSchema } from '@repo/data-ops/zod-schema/links';
+
 import { Hono } from 'hono';
 
 export const App = new Hono<{ Bindings: Env }>();
 
 App.get('/:id', async (c) => {
-	console.log(JSON.stringify(c.req.raw.cf));
-	const cf = c.req.raw.cf;
-	const country = cf?.country;
-	const lat = cf?.latitude;
-	const long = cf?.longitude;
-	return c.json({
-		country,
-		lat,
-		long,
-	});
+	const id = c.req.param('id');
+
+	const linkInfo = await getLink(id);
+	if (!linkInfo) {
+		return c.text('Destination not found', 404);
+	}
+
+	const cfHeader = cloudflareInfoSchema.safeParse(c.req.raw.cf);
+	if (!cfHeader.success) {
+		return c.text('Invalid Cloudflare headers', 400);
+	}
+
+	const headers = cfHeader.data;
+	const destination = getDestinationForCountry(linkInfo, headers.country);
+	return c.redirect(destination);
 });
